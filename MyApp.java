a2b6fd1cd646a5a55d3f1276413ae7e2258057e2
JaeHyeokLee <wogurlee@naver.com>
2017-04-06 02:01:04 +0900

    First Commit of b2 - make changePrice

--- MyApp.java
+++ MyApp.java
@@ -1,7 +1,10 @@
 public class main {
  public static void main(String[] args) {
   Product product = new Product("Edam", 3.3, 120);
-  System.out.println("Product value is " + product.countValue());
+  System.out.println("Current Product value is " + product.countValue());
+  product.printProduct();
+  product.changePrice(3.0);
+  System.out.println("After Change price, Current Product value is " + product.countValue());
   product.printProduct();
  }
 }
@@ -16,6 +19,9 @@ class Product {
  }
  public double countValue() {
   return (amount * price);
+ }
+ public void changePrice(double newPrice){
+  this.price = newPrice;
  }
  public void printProduct() {
   System.out.printf("Product %s, price %4.1f and balance %d pcs", name, price, amount);
